c0e4d6be6e89d357e1a3da9b01c803ac960b75a6
Wil Cooley <wcooley@nakedape.cc>
2013-11-14 22:44:35 -0800

    Revert to simpler puppetlabs_spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,19 +1,2 @@
-dir = File.expand_path(File.dirname(__FILE__))
-$LOAD_PATH.unshift File.join(dir, 'lib')
-
-require 'puppet'
-require 'mocha/api'
-require 'rspec'
-
-require 'rubygems'
-require 'puppetlabs_spec_helper/rake_tasks'
-
-RSpec.configure do |config|
-    config.mock_with :mocha
-end
-
-# We need this because the RAL uses 'should' as a method.  This
-# allows us the same behaviour but with a different method name.
-class Object
-    alias :must :should
-end
+require 'rspec-puppet'
+require 'puppetlabs_spec_helper/module_spec_helper'
